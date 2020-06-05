e510677ef446cac34b4930b57cd6c481f0307542
Pauper33 <skurkodenis@gmail.com>
2020-06-05 06:48:49 +0300

    add function of the send messsages

--- client/client.js
+++ client/client.js
@@ -4,11 +4,25 @@ const socket = io();
 
 const getEl = e => document.getElementById(e);
 
-let username = '';
+let name = '';
 let message = '';
 
-const usernameField = getEl('username');
-const messageOut = getEl('messages');
-const messageIn = getEl('send-message');
-const sendBt = getEl('send');
+const fild_username = getEl('username');
+const fild_message = getEl('messages');
+const fild_send_message = getEl('send-message');
+const button_send = getEl('send');
+
+button_send.addEventListener('click', () => {
+  sendMessage();
+});
+
+const sendMessage = () => {
+  socket.emit('Send message', { message: fild_send_message.value });
+  fild_send_message.value = '';
+}
+
+socket.on('Add message', (data) => {
+  fild_message.value += data.message;
+});
+
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const port = 8080;
+const port = 3001;
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(__dirname + '/client/index.html'));
 
-
-
 io.sockets.on('connection', function(socket) {
   console.log("Connect " + socket.id);
   connection.push(socket);
@@ -25,7 +23,11 @@ io.sockets.on('connection', function(socket) {
   socket.on('disconnect', function(data) {
     connection.slice(connection.indexOf(socket), 1)
     console.log("Disconnect " + socket.id)
-  })
+  });
+
+  socket.on('Send message', (data) => {
+    io.sockets.emit('Add message', data);
+  });
 });
 
 
